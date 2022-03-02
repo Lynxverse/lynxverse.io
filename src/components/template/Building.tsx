531c4cb13289e6c0aa74c331f92c01f99851f42c
fiqryq <fiqrychoerudin48@gmail.com>
2022-03-02 20:30:20 +0700

    fix building section

--- src/components/template/Building.tsx
+++ src/components/template/Building.tsx
@@ -4,10 +4,7 @@ import { IMAGE } from "utils/images";
 const Building = () => {
   return (
     <Section size={false}>
-      <div className="relative">
-        <img className="mx-auto py-10" src={IMAGE.building} alt="building" />
-        <div className="h-[500px] w-[500px] rounded-full bg-[#1AC7FE]/60"></div>
-      </div>
+      <img className="mx-auto py-10" src={IMAGE.building} alt="building" />
     </Section>
   );
 };
