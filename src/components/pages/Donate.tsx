bfa34c178518ef86aff4c59bba16f0ea3440952f
Hardin Santoso <hardinzuhdisantoso@gmail.com>
2022-04-22 16:46:00 +0700

    Front end update

--- src/components/pages/Donate.tsx
+++ src/components/pages/Donate.tsx
@@ -59,28 +59,44 @@ console.log(gas.toString())
 console.log(result);
   }
   return (
-    <div className="w-full flex flex-col justify-center items-center pt-[100px]">
-      <div className="flex flex-col justify-center align-center pt-[100px] w-[200px]">
-        <span className="text-lg text-white mt-6">Input the UST Amount</span>
+    <div className="w-full flex flex-col justify-center items-center pt-[120px]">
+      
+      <span className="text-2xl md:text-4xl text-white mt-6 justify-center font-sans mb-8">Donate to Lynxverse</span>
+      <div className="bg-sky-900 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div className="flex flex-col justify-center align-center  w-[240px] md:w-[440px]" >
+      <span className="text-lg text-white mt-6 justify-center">Via Terra Station</span>
         <input 
-          className="mt-6" 
+          className="mt-6 appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name " 
           type="text" 
           value={ustAmount} 
           onChange ={(e) => {setUstAmount(e.target.value)}}
+          placeholder="0 UST"
         />
-        <button className="bg-slate-200 mt-6" onClick={() => {donate_ust()}}>Donate</button>
+        <button className="mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={() => {donate_ust()}}>Donate</button>
       </div>
 
-      <div className="flex flex-col justify-center align-center pt-[100px] w-[200px]">
-        <span className="text-lg text-white mt-6">Input the ETH Amount</span>
+      <div className="flex flex-col justify-center align-center pt-[50px] w-[240px] md:w-[440px]">
+      <span className="text-lg text-white mt-6 justify-center">Via Metamask with Ethereum</span>
         <input 
-          className="mt-6" 
+          className="mt-6 appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" 
           type="text" 
           value={ethAmount} 
           onChange ={(e) => {setEthAmount(e.target.value)}}
+          placeholder="0 ETH"
         />
-        <button className="bg-slate-200 mt-6" onClick={() => {donate_eth()}}>Donate</button>
+        <button className="mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={() => {donate_eth()}}>Donate</button>
       </div>
+      </div>
+      <div className="bg-sky-200 border-t-4 border-cyan-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+  <div className="flex">
+    <div className="py-1"><svg className="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+    <div>
+      <p className="font-bold">Donating to Lynxverse</p>
+      <p className="text-sm">Be Advised you are not to be compensated for your donation. <br/>Your donation will be used for advancing charitable
+      idea of Lynxverse</p>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
