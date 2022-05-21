372360e412fa7decbd1567f22edde64f9d37af11
Hardin Santoso <hardinzuhdisantoso@gmail.com>
2022-05-21 22:40:46 +0700

    Change Font

--- src/components/template/Video-Campaign.tsx
+++ src/components/template/Video-Campaign.tsx
@@ -16,10 +16,10 @@ const Videocampaign = () => {
 <div className=" flex relative z-20 items-center">
     <div className="container mx-auto px-6 flex flex-col justify-between items-center relative py-8">
         <div className="flex flex-col">
-            <h1 className="font-title w-full uppercase text-center text-4xl sm:text-5xl dark:text-white text-gray-800">
+            <h1 className="font-title text-center text-4xl sm:text-5xl dark:text-white text-gray-800">
                 Our Current Campaign - Maja x Lynxverse
             </h1>
-            <h2 className="font-description max-w-2xl mx-auto w-full text-xl dark:text-white text-gray-500 text-center py-8">
+            <h2 className="font-description text-[1rem] leading-relaxed text-[#D8D8D8] md:text-description  max-w-4xl mx-auto w-full text-center py-8">
                Lynxverse partnering with Maja to offer novel theraphy for those in needs such as those in crypto space that needed support in recent Luna spiral and crypto and stock market volatility. 
                <br/> Maja offers therapies such as hypno theraphy
             </h2>
