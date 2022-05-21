476fb4c5447c84481f0f259fb4858e3d8356de44
Hardin Santoso <hardinzuhdisantoso@gmail.com>
2022-05-21 21:12:36 +0700

    Adding video

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -8,11 +8,13 @@ import { Subscribe } from "components/template/Subscribe";
 import { Footer } from "components/template/Footer";
 import { Income } from "components/template/Income";
 import { Illustration } from "components/template/Illustration";
+import { Videocampaign } from "components/template/Video-Campaign";
 const LandingPage = () => {
   return (
     <div className="w-full overflow-hidden bg-gradient-to-b from-[#081850] via-[#081850] to-[#2B99B1]">
       <Hero />
       <About />
+      <Videocampaign />
       <Unique />
       <Building />
       <Illustration />
