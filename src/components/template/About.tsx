9bdf6c3681663e49e009e804d0ffd0d94beda7bf
Hardin Santoso <hardinzuhdisantoso@gmail.com>
2022-06-02 23:33:36 +0700

    Change whitepaper

--- src/components/template/About.tsx
+++ src/components/template/About.tsx
@@ -34,7 +34,7 @@ const About = () => {
             distress among different generations and cultures. These mental
             health issues are often overlooked due to many factors in the space.
             <br/>
-            Lynxverse comes as a solutuion, offering Cognitive Behavioral Therapy (CBT) services 
+            Lynxverse comes as a solution offering Cognitive Behavioral Therapy (CBT) services 
             implemented on Virtual Reality to help and raise awareness of mental health and well-being.
             One of the first on the blockchain focused on such matters with 
             X to Earn system where users can earn stable coin. The “earn” is designed to fully utilize Lynxverse.
